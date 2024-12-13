442b75d028c3a5d17722c5dcf56a09bcf1ff04a6
Dima <dimaklim04@gmail.com>
2024-12-13 20:43:07 +0200

    fix route in verify

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -3,41 +3,43 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   try {
-    let code: string | null = null;
-    code = req.nextUrl.searchParams.get('code');
+    // Получение параметра "code"
+    const code = req.nextUrl.searchParams.get('code');
 
     if (!code) {
       return NextResponse.json({ error: 'Неверный код' }, { status: 400 });
     }
 
+    // Поиск verificationCode в базе данных
     const verificationCode = await prisma.verificationCode.findFirst({
-      where: {
-        code,
-      },
+      where: { code },
     });
 
     if (!verificationCode) {
-      return NextResponse.json({ error: 'Неверный код' }, { status: 400 });
+      return NextResponse.json({ error: 'Код не найден' }, { status: 404 });
     }
 
+    // Обновление пользователя как "проверенного"
     await prisma.user.update({
-      where: {
-        id: verificationCode.userId,
-      },
-      data: {
-        verified: new Date(),
-      },
+      where: { id: verificationCode.userId },
+      data: { verified: new Date() },
     });
 
+    // Удаление verificationCode после использования
     await prisma.verificationCode.delete({
-      where: {
-        id: verificationCode.id,
-      },
+      where: { id: verificationCode.id },
     });
 
-    return NextResponse.redirect(new URL('/?verified', req.url));
+    // Перенаправление на главную страницу с параметром "verified"
+    const redirectUrl = new URL('/?verified=true', req.url);
+    return NextResponse.redirect(redirectUrl);
   } catch (error) {
-    console.error(error);
-    console.log('[VERIFY_GET] Server error', error);
+    console.error('[VERIFY_GET] Server error', error);
+
+    // Возвращаем ошибку при сбое
+    return NextResponse.json(
+      { error: 'Произошла внутренняя ошибка сервера' },
+      { status: 500 }
+    );
   }
 }
